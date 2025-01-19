377afa53d8c5138830a76da2c2885d7e1306b89e
EmmaAdabra <adabraemma@gmail.com>
2025-01-19 21:17:50 +0000

    Add BuildSqlStatement class
    - A utility class that set PrepareStatement parameters

---
+++ src/main/java/com/adb/chat_app/utils/BuildSqlStatement.java
@@ -0,0 +1,14 @@
+package com.adb.chat_app.utils;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class BuildSqlStatement {
+    public static PreparedStatement setParameters(PreparedStatement ps, Object ... params) throws SQLException {
+        for(int i = 0; i < params.length; i++){
+            ps.setObject(i + 1, params[i]);
+        }
+
+        return ps;
+    }
+}
