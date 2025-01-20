c1cfdb96aafc53e2e0fd8510febf9036fe3b6c6a
EmmaAdabra <adabraemma@gmail.com>
2025-01-20 00:40:17 +0000

    Modified sql scripts directory path

--- src/main/java/com/adb/chat_app/dao/userdao/UserSqlScriptsPath.java
+++ src/main/java/com/adb/chat_app/dao/userdao/UserSqlScriptsPath.java
@@ -1,17 +1,18 @@
 package com.adb.chat_app.dao.userdao;
 
 public enum UserSqlScriptsPath {
+
     INSERT_USER_SCRIPT(getUserScriptDir() + "insert_user.sql");
 
-    private static String USER_SCRIPT_DIR = "src/sql_scripts/user_scripts/";
     private String path;
 
+    private final static String sqrScriptsDir = "sql_scripts/userscripts/";
     UserSqlScriptsPath(String path) {
         this.path = path;
     }
 
-    public static String getUserScriptDir() {
-        return USER_SCRIPT_DIR;
+    public static String  getUserScriptDir() {
+        return sqrScriptsDir;
     }
 
     public String getPath() {
