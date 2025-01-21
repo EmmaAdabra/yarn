3e1b0f1c00eda861103a43fbac52721ca4ea33c9
EmmaAdabra <adabraemma@gmail.com>
2025-01-21 04:40:02 +0000

    Implement doPost method in RegisterUserServlet
    - Get user registration details
    - Instantiate user Object

--- src/main/java/com/adb/chat_app/controllers/RegisterUserServlet.java
+++ src/main/java/com/adb/chat_app/controllers/RegisterUserServlet.java
@@ -3,6 +3,7 @@ package com.adb.chat_app.controllers;
 import com.adb.chat_app.dao.userdao.IUserDao;
 import com.adb.chat_app.dao.userdao.UserDao;
 import com.adb.chat_app.exceptions.DAOException;
+import com.adb.chat_app.exceptions.InputValidationException;
 import com.adb.chat_app.models.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,12 +19,24 @@ public class RegisterUserServlet extends HttpServlet {
     private IUserDao userDao = new UserDao();
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+    protected void doGet(
+            HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException
+    {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/views/registerUser.jsp");
+        dispatcher.forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String firstName, lastName, username, email, password, confirmPassword;
 
+        firstName = request.getParameter("firstName");
+        lastName = request.getParameter("lastName");
+        username = request.getParameter("username");
+        email = request.getParameter("email");
+        password = request.getParameter("password");
+
+        User user = new User(firstName, lastName, username, email, password);
     }
 }
