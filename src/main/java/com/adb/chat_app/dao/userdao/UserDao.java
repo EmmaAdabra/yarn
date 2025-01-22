222c538e4f32d64bbdd100459ad763a0690c5e57
EmmaAdabra <adabraemma@gmail.com>
2025-01-22 08:52:27 +0000

    Add a new field to User class - createdAt
    - Type - Timestamp

--- src/main/java/com/adb/chat_app/dao/userdao/UserDao.java
+++ src/main/java/com/adb/chat_app/dao/userdao/UserDao.java
@@ -3,6 +3,7 @@ package com.adb.chat_app.dao.userdao;
 import com.adb.chat_app.exceptions.DAOException;
 import com.adb.chat_app.models.User;
 import com.adb.chat_app.utils.CreateDbConnection;
+import com.adb.chat_app.utils.EntityModelMapper;
 import com.adb.chat_app.utils.GetSqlStatement;
 import com.adb.chat_app.utils.BuildSqlStatement;
 import org.slf4j.Logger;
@@ -68,6 +69,8 @@ public class UserDao implements IUserDao{
                     userID = resultSet.getInt("id");
                 }
 
+                logger.info("insert user query executed successfully");
+
             } catch (SQLException e){
                 logger.error("SQL execution error: " + e.getMessage(), e);
                 throw new DAOException("sql execution error", e);
@@ -90,5 +93,50 @@ public class UserDao implements IUserDao{
     @Override
     public int delete(long id) throws DAOException {
         return 0;
+    }
+
+
+    @Override
+    public User findUserByEmail(String email) throws DAOException {
+        User user = null;
+
+        logger.info("connecting to database");
+        try(Connection connection = CreateDbConnection.getConnection()){
+            logger.info("Database connected successfully");
+
+            String getUserByEmailSqlQuery;
+            String sqlScriptPath = UserSqlScriptsPath.GET_USER_BY_EMAIL.getPath();
+
+            logger.info("fetching get user by email sql query from " + sqlScriptPath);
+            try{
+                getUserByEmailSqlQuery = GetSqlStatement.sqlQueryBuilder(sqlScriptPath);
+                logger.info("get user by email sql query fetched successfully");
+            }  catch (IOException e) {
+                logger.error("Failed to fetch get user by email sql query from " + sqlScriptPath, e);
+                throw new DAOException("Failed to fetch get user by email sql query", e);
+            }
+
+            try(PreparedStatement preparedStatement = connection.prepareStatement(getUserByEmailSqlQuery)){
+                preparedStatement.setString(1, email);
+
+                ResultSet resultSet = preparedStatement.executeQuery();
+
+                if(resultSet.next()){
+                    user = EntityModelMapper.userMapper(resultSet);
+                }
+
+                logger.info("get user by email query executed successfully");
+
+            } catch (SQLException e){
+                logger.error("SQL execution error: " + e.getMessage(), e);
+                throw new DAOException("sql execution error", e);
+            }
+
+        } catch (SQLException e){
+            logger.error("Fail to connect to database: " + e.getMessage(), e);
+            throw new DAOException("Fail to connect to database", e);
+        }
+
+        return user;
     }
 }
