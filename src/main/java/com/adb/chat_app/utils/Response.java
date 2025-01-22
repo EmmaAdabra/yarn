e0115cdfa9ab089e85550c086f9d1525b077725f
EmmaAdabra <adabraemma@gmail.com>
2025-01-22 08:51:02 +0000

    Add getters methods

--- src/main/java/com/adb/chat_app/utils/Response.java
+++ src/main/java/com/adb/chat_app/utils/Response.java
@@ -14,5 +14,19 @@ public class Response<T> {
     public Response(int status_code, String message) {
         this.status_code = status_code;
         this.message = message;
+
     }
+
+    public int getStatus_code() {
+        return status_code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
 }
