70aa610bae69e6c3833e5fd62c497d223e7600e6
EmmaAdabra <adabraemma@gmail.com>
2025-01-22 15:33:48 +0000

    Add UnknownException class
    - Use to categorize uncaught errors

---
+++ src/main/java/com/adb/chat_app/exceptions/UnknownException.java
@@ -0,0 +1,7 @@
+package com.adb.chat_app.exceptions;
+
+public class UnknownException extends Exception{
+    public UnknownException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
