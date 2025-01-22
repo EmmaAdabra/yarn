8239037d83fe71c00870f70ddecae14181baa2df
EmmaAdabra <adabraemma@gmail.com>
2025-01-22 08:49:02 +0000

    Add a new contract method - getUserByEmail
    - Accept string
    - Returns User object or null

--- src/main/java/com/adb/chat_app/dao/userdao/IUserDao.java
+++ src/main/java/com/adb/chat_app/dao/userdao/IUserDao.java
@@ -1,8 +1,11 @@
 package com.adb.chat_app.dao.userdao;
 
 import com.adb.chat_app.dao.IDAO;
+import com.adb.chat_app.exceptions.DAOException;
 import com.adb.chat_app.models.User;
 
-public interface IUserDao extends IDAO<User> {
+import java.util.Optional;
 
+public interface IUserDao extends IDAO<User> {
+    User findUserByEmail(String email) throws DAOException;
 }
