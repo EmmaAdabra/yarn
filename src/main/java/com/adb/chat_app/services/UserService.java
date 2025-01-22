6e4ad413d5c5ce5e43eb042c09356131470cd52e
EmmaAdabra <adabraemma@gmail.com>
2025-01-22 15:32:46 +0000

    Refactor
    - Refactor all methods to throw UnknownException

--- src/main/java/com/adb/chat_app/services/UserService.java
+++ src/main/java/com/adb/chat_app/services/UserService.java
@@ -2,13 +2,12 @@ package com.adb.chat_app.services;
 
 import com.adb.chat_app.dao.userdao.IUserDao;
 import com.adb.chat_app.exceptions.DAOException;
+import com.adb.chat_app.exceptions.UnknownException;
 import com.adb.chat_app.models.User;
 import com.adb.chat_app.utils.Response;
 import com.adb.chat_app.utils.ResponseCode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Optional;
 
 public class UserService {
     private final Logger logger = LoggerFactory.getLogger(UserService.class);
@@ -18,7 +17,7 @@ public class UserService {
         this.userDao = userDao;
     }
 
-    public Response<User> getUserByEmail(String email){
+    public Response<User> getUserByEmail(String email) throws UnknownException {
         try {
             User user = userDao.findUserByEmail(email);
 
@@ -28,23 +27,26 @@ public class UserService {
             }
             logger.warn("User with email - {} not found in database", email);
         } catch (DAOException e) {
-            logger.error("Error occur fetching user with email {}", email);
+            logger.error("Error occur fetching user with email {}", email, e);
             return  new Response<>(ResponseCode.INTERNAL_SERVER_ERROR.getCode(), "Internal server error");
         }
 
-        return new Response<>(ResponseCode.RESOURCE_NOT_FOUND.getCode(), "user not found");
+        return new Response<>(ResponseCode.RESOURCE_NOT_FOUND.getCode(), "Internal server error");
     }
 
-    public Response<Integer> saveUser(User user) {
+    public Response<Integer> saveUser(User user) throws UnknownException{
+        Response response;
+
         try {
            int userID = userDao.save(user);
            logger.info("successfully save user with ID - {} to database", userID);
 
-           return new Response<>(ResponseCode.RESOURCE_CREATED.getCode(), "User added to database", userID);
+           response = new Response<>(ResponseCode.RESOURCE_CREATED.getCode(), "User added to database", userID);
         } catch (DAOException e) {
             logger.error("Error occur while saving user to database");
-
-            return new Response<>(ResponseCode.INTERNAL_SERVER_ERROR.getCode(), "user not save to database");
+            response = new Response<>(ResponseCode.INTERNAL_SERVER_ERROR.getCode(), "user not save to database");
         }
+
+        return response;
     }
 }
