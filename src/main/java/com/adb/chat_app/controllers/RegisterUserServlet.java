a222ba4ea7243d668322c041c9367167e511dbfc
EmmaAdabra <adabraemma@gmail.com>
2025-01-22 15:29:56 +0000

    Add a catch block for unknown exceptions

--- src/main/java/com/adb/chat_app/controllers/RegisterUserServlet.java
+++ src/main/java/com/adb/chat_app/controllers/RegisterUserServlet.java
@@ -3,6 +3,8 @@ package com.adb.chat_app.controllers;
 import com.adb.chat_app.dao.userdao.UserDao;
 import com.adb.chat_app.models.User;
 import com.adb.chat_app.services.UserService;
+import com.adb.chat_app.utils.GlobalErrorHandler;
+import com.adb.chat_app.utils.LogPath;
 import com.adb.chat_app.utils.Response;
 import com.adb.chat_app.utils.ResponseCode;
 import org.slf4j.Logger;
@@ -32,25 +34,32 @@ public class RegisterUserServlet extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String firstName, lastName, username, email, password, confirmPassword;
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        String firstName, lastName, username, email, password;
 
-        firstName = request.getParameter("firstName");
-        lastName = request.getParameter("lastName");
-        username = request.getParameter("username");
-        email = request.getParameter("email");
-        password = request.getParameter("password");
+        try{
+            firstName = request.getParameter("firstName");
+            lastName = request.getParameter("lastName");
+            username = request.getParameter("username");
+            email = request.getParameter("email");
+            password = request.getParameter("password");
 
-        User user = new User(firstName, lastName, username, email, password);
+            User user = new User(firstName, lastName, username, email, password);
 
-        Response<Integer> serviceResponse  = userService.saveUser(user);
+            Response<Integer> serviceResponse  = userService.saveUser(user);
 
-        if(serviceResponse.getStatus_code() == ResponseCode.RESOURCE_CREATED.getCode()){
-            System.out.println("Account created successfully");
-        }
+            if(serviceResponse.getStatus_code() == ResponseCode.RESOURCE_CREATED.getCode()){
+                System.out.println("Account created successfully");
+            }
 
-        if(serviceResponse.getStatus_code() == ResponseCode.INTERNAL_SERVER_ERROR.getCode()){
-            System.out.println("Fail to create Account");
+            if(serviceResponse.getStatus_code() == ResponseCode.INTERNAL_SERVER_ERROR.getCode()){
+                System.out.println("Fail to create Account");
+            }
+        } catch (Exception e){
+            GlobalErrorHandler.handleError(e);
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Internal server error occur");
         }
     }
 }
