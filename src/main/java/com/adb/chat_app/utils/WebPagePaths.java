8ff30f7fdc179d67c3095f2b1a7df7946322a6f4
EmmaAdabra <adabraemma@gmail.com>
2025-01-26 02:43:48 +0000

    Add WebPagePaths enum
    - Enum for managing web page paths in the "WEB-INF/views" directory
    - Provides type-safe constants and methods for retrieving full JSP paths.

---
+++ src/main/java/com/adb/chat_app/utils/WebPagePaths.java
@@ -0,0 +1,24 @@
+package com.adb.chat_app.utils;
+
+public enum WebPagePaths {
+    REGISTER(getPathDir() + "registerUser.jsp"),
+    LOGIN(getPathDir() + "login.jsp"),
+    DASHBOARD(getPathDir() + "dashboard.jsp")
+    ;
+
+    private final String pagePath;
+    private static final  String  PATH_DIR = "WEB-INF/views/";
+
+    WebPagePaths(String pagePath) {
+        this.pagePath = pagePath;
+    }
+
+    public String getPagePath(){
+        return pagePath;
+    }
+
+    public static String getPathDir(){
+        return PATH_DIR;
+    }
+}
+
