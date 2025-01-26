de2a68fb4fda4a95d475dcb87fd38929f439a0a0
EmmaAdabra <adabraemma@gmail.com>
2025-01-26 02:06:35 +0000

    Add AuthenticationFilter class
    - This authenticates user before accessing protected route

---
+++ src/main/java/com/adb/chat_app/filters/AuthenticationFilter.java
@@ -0,0 +1,26 @@
+package com.adb.chat_app.filters;
+
+import com.adb.chat_app.utils.WebPagePaths;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter({"/dashboard"})
+public class AuthenticationFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+
+        HttpSession session = request.getSession(false);
+
+        if(session != null && session.getAttribute("userID") != null){
+            filterChain.doFilter(request, servletResponse);
+        } else {
+            ((HttpServletResponse) servletResponse).sendRedirect("/login");
+        }
+    }
+}
