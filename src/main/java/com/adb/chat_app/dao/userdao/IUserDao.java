d6a3810fb9637249462d7f86cf3cebff67281dbb
EmmaAdabra <adabraemma@gmail.com>
2025-02-14 21:32:38 +0000

    Modify IUserDao interface
     - Add the following contacts methods
    - Method for saving user pfp
    - Method to check if a user have a pfp
    - Method to fetch user pfp
    - Method to update user bio

--- src/main/java/com/adb/chat_app/dao/userdao/IUserDao.java
+++ src/main/java/com/adb/chat_app/dao/userdao/IUserDao.java
@@ -4,8 +4,13 @@ import com.adb.chat_app.dao.IDAO;
 import com.adb.chat_app.exceptions.DAOException;
 import com.adb.chat_app.models.User;
 
+import javax.servlet.http.Part;
 import java.util.Optional;
 
 public interface IUserDao extends IDAO<User> {
     User findUserByEmail(String email) throws DAOException;
+    boolean saveUserPfp(long userId, Part imagePart) throws DAOException;
+    boolean hasUserPfp(long userID) throws DAOException;
+    byte[] fetchUserPfp(long userId) throws DAOException;
+    int updateUserBio(String bio, int userId) throws DAOException;
 }
