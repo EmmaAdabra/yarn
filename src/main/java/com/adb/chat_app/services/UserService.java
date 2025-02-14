91fb2bf767d044e973bc0205a73f71fc0a9c763c
EmmaAdabra <adabraemma@gmail.com>
2025-02-14 21:37:54 +0000

    Modify UserDao class
     - add define methods for the following:
    - Method for saving user pfp
    - Method to check if a user have a pfp
    - Method to fetch user pfp
    - Method to update user bio
    - Method to create sessionUser object

--- src/main/java/com/adb/chat_app/services/UserService.java
+++ src/main/java/com/adb/chat_app/services/UserService.java
@@ -1,13 +1,17 @@
 package com.adb.chat_app.services;
 
 import com.adb.chat_app.dao.userdao.IUserDao;
+import com.adb.chat_app.dto.SessionUserDTO;
 import com.adb.chat_app.exceptions.DAOException;
 import com.adb.chat_app.exceptions.UnknownException;
 import com.adb.chat_app.models.User;
 import com.adb.chat_app.utils.Response;
 import com.adb.chat_app.utils.ResponseCode;
+import com.adb.chat_app.utils.StringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.Part;
 
 public class UserService {
     private final Logger logger = LoggerFactory.getLogger(UserService.class);
@@ -65,5 +69,82 @@ public class UserService {
         }
 
         return response;
-}
+    }
+
+    public Response<SessionUserDTO> createSessionUser(User user) throws UnknownException {
+        SessionUserDTO sessionUser = new SessionUserDTO();
+        sessionUser.setUserID(user.getId());
+        sessionUser.setfName(user.getFirstName());
+        sessionUser.setlName(user.getLastName());
+        sessionUser.setEmail(user.getEmail());
+        sessionUser.setUsername(user.getUsername());
+        sessionUser.setUserFullName(StringUtil.getUSerFullName(user.getFirstName(), user.getLastName()));
+
+        if(user.getBio() != null){
+            sessionUser.setBio(user.getBio());
+        }
+
+
+        if(hasPfp(user.getId())) {
+            sessionUser.setHasPfp(true);
+        } else {
+            sessionUser.setUserInitial(StringUtil.getUserInitial(user.getFirstName(), user.getLastName()));
+        }
+
+
+
+        return new Response<>(ResponseCode.SUCCESS.getCode(), "Success", sessionUser);
+    }
+
+    public Response<Object> uploadUserPfp(long userId, Part imagePart) throws UnknownException{
+
+        try{
+            if(userDao.saveUserPfp(userId, imagePart)){
+                return new Response<>(ResponseCode.RESOURCE_CREATED.getCode(), "User profile picture uploaded");
+            }
+        } catch (DAOException e){
+            return new Response<>(ResponseCode.INTERNAL_SERVER_ERROR.getCode(), "Could not upload image, internal server error");
+        }
+        return new Response<>(101, "Profile picture not uploaded due to an unknown or silent error");
+    }
+
+    private boolean hasPfp(long userId) throws UnknownException{
+
+        try{
+            return userDao.hasUserPfp(userId);
+        } catch (DAOException e) {
+            logger.error("Error occur checking if user have a pfp -- {}", e.getMessage(), e);
+            return false;
+        }
+    }
+
+    public Response<byte[]> fetchPfp(long userId) throws UnknownException {
+        try{
+            byte[] userPfp = userDao.fetchUserPfp(userId);
+            if(userPfp != null || userPfp.length != 0){
+                return new Response<>(ResponseCode.SUCCESS.getCode(), "Success", userPfp);
+            }
+        } catch (DAOException e) {
+            logger.error("Failed to fetch user profile picture -- {}", e.getMessage(), e);
+            return new Response<>(ResponseCode.INTERNAL_SERVER_ERROR.getCode(), "Failed to fetch user profile picture", new byte[0]);
+        }
+
+        return new Response<>(ResponseCode.RESOURCE_NOT_FOUND.getCode(), "user have no pfp", new byte[0]);
+    }
+
+    public Response<Integer> addBio(String bio, long userId) throws UnknownException{
+        try{
+            int updateBio = userDao.updateUserBio(bio, (int) userId);
+            if(updateBio != 0){
+                return new Response<>(ResponseCode.RESOURCE_CREATED.getCode(), "User bio updated");
+            }
+        } catch (DAOException e) {
+            logger.error("Failed to fetch user profile picture -- {}", e.getMessage(), e);
+            return new Response<>(ResponseCode.INTERNAL_SERVER_ERROR.getCode(), "Failed to update user bio, an internal error occur");
+        } catch (Exception e){
+            return new Response<>(ResponseCode.INTERNAL_SERVER_ERROR.getCode(), "Unknown error while trying to update user bio");
+        }
+
+        return new Response<>(ResponseCode.RESOURCE_NOT_FOUND.getCode(), "User not found");
+    }
 }
--- src/main/java/com/adb/chat_app/utils/UserUtil.java
+++ src/main/java/com/adb/chat_app/utils/UserUtil.java
@@ -5,6 +5,9 @@ import com.adb.chat_app.exceptions.UnknownException;
 import com.adb.chat_app.models.User;
 import com.adb.chat_app.services.UserService;
 
+import javax.servlet.http.HttpServletRequest;
+import java.nio.file.Path;
+
 public class UserUtil {
     private static final UserService userService = new UserService(new UserDao());
 
@@ -14,4 +17,7 @@ public class UserUtil {
         return serviceResponse.getStatus_code() == ResponseCode.SUCCESS.getCode();
     }
 
+    public static String getUserPfpUrl(String appPath, long userId){
+        return appPath + "/fetchPfp?id=" + userId;
+    }
 }
