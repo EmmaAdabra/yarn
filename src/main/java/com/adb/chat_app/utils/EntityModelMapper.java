982cf82545d5240433087eb397bf2eee5ad3f1c5
EmmaAdabra <adabraemma@gmail.com>
2025-02-14 21:57:26 +0000

    Add methods that maps Post entity to Post DTO

--- src/main/java/com/adb/chat_app/utils/EntityModelMapper.java
+++ src/main/java/com/adb/chat_app/utils/EntityModelMapper.java
@@ -1,5 +1,8 @@
 package com.adb.chat_app.utils;
 
+import com.adb.chat_app.dto.PostDto;
+import com.adb.chat_app.dto.PosterDto;
+import com.adb.chat_app.models.Post;
 import com.adb.chat_app.models.User;
 
 import java.sql.ResultSet;
@@ -21,4 +24,42 @@ public class EntityModelMapper  {
 
         return user;
     }
+
+    public static PostDto postMapper(ResultSet resultSet) throws SQLException {
+        PostDto postDto = new PostDto();
+        postDto.setPostId(resultSet.getInt("post_id"));
+        postDto.setPostTile(resultSet.getString("title"));
+        postDto.setContent(resultSet.getString("content"));
+        postDto.setMedia(resultSet.getString("media_path"));
+        postDto.setPostDate(DateUtil.formatPostDate(resultSet.getTimestamp("created_at")));
+
+        return postDto;
+    }
+
+    public static PosterDto posterMapper(String appPath, ResultSet resultSet) throws SQLException {
+        PosterDto posterDto = new PosterDto();
+        int posterId = resultSet.getInt("user_id");
+
+        posterDto.setId(posterId);
+        posterDto.setFirstName(resultSet.getString("first_name"));
+        posterDto.setName(
+                StringUtil.getUSerFullName(
+                        resultSet.getString("first_name"),
+                        resultSet.getString("last_name")
+                )
+        );
+        posterDto.setInitial(
+                StringUtil.getUserInitial(
+                        resultSet.getString("first_name"),
+                        resultSet.getString("last_name")
+                )
+        );
+
+        if(resultSet.getInt("has_pfp") == 1){
+            posterDto.setPfp(UserUtil.getUserPfpUrl(appPath, posterId));
+        }
+
+        return posterDto;
+    }
+
 }
