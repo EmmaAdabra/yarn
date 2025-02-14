49a2718b8f7424125a940f8f5c303eac41da42c2
EmmaAdabra <adabraemma@gmail.com>
2025-02-14 22:36:28 +0000

    Update WebPagePaths to contain new path

--- src/main/java/com/adb/chat_app/utils/WebPagePaths.java
+++ src/main/java/com/adb/chat_app/utils/WebPagePaths.java
@@ -3,8 +3,8 @@ package com.adb.chat_app.utils;
 public enum WebPagePaths {
     REGISTER(getPathDir() + "registerUser.jsp"),
     LOGIN(getPathDir() + "login.jsp"),
-    DASHBOARD(getPathDir() + "dashboard.jsp")
-    ;
+    DASHBOARD(getPathDir() + "dashboard.jsp"),
+    EDIT_USER_PROFILE(getPathDir() + "editUserProfile.jsp");
 
     private final String pagePath;
     private static final  String  PATH_DIR = "WEB-INF/views/";
