380a8f10c951cfdb4f4b3352aefe46b7a1b5643b
EmmaAdabra <adabraemma@gmail.com>
2025-02-14 21:33:54 +0000

    Modify UserDao class
     - implement the following contacts methods
    - Method for saving user pfp
    - Method to check if a user have a pfp
    - Method to fetch user pfp
    - Method to update user bio

--- src/main/java/com/adb/chat_app/dao/userdao/UserDao.java
+++ src/main/java/com/adb/chat_app/dao/userdao/UserDao.java
@@ -9,7 +9,9 @@ import com.adb.chat_app.utils.BuildSqlStatement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.http.Part;
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -137,5 +139,130 @@ public class UserDao implements IUserDao{
         }
 
         return user;
+    }
+
+    @Override
+    public boolean saveUserPfp(long userId, Part imagePart) throws DAOException {
+        logger.info("connecting to database");
+        try(Connection connection = CreateDbConnection.getConnection()){
+            logger.info("Database connected successfully");
+
+            String insertPfpQuery;
+            String sqlScriptPath = UserSqlScriptsPath.INSERT_USER_PFP.getPath();
+
+            insertPfpQuery = GetSqlStatement.sqlQueryBuilder(sqlScriptPath);
+
+            try(PreparedStatement preparedStatement = connection.prepareStatement(insertPfpQuery);
+                InputStream fileContent =  imagePart.getInputStream()){
+                preparedStatement.setBinaryStream(1, fileContent);
+                preparedStatement.setInt(2, (int) userId);
+
+                int rowInserted = preparedStatement.executeUpdate();
+
+                if(rowInserted > 0){
+                    return true;
+                } else {
+                    logger.warn("Failed to save profile picture");
+                }
+            } catch (SQLException e){
+                logger.error("SQL execution error: " + e.getMessage(), e);
+                throw new DAOException("sql execution error", e);
+            }
+
+
+        } catch (SQLException e) {
+            logger.error("Fail to connect to database: " + e.getMessage(), e);
+            throw new DAOException("Fail to connect to database", e);
+        } catch (IOException e){
+            logger.error("Failed to fetch get insert user pfp sql query -- " + e.getMessage());
+            throw new DAOException("Failed to fetch get insert user pfp sql query", e);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean hasUserPfp(long userID) throws DAOException {
+        try(Connection connection = CreateDbConnection.getConnection()){
+            String sqlScriptPath = UserSqlScriptsPath.HAS_USER_PFP.getPath();
+            String hasUserPfpQuery = GetSqlStatement.sqlQueryBuilder(sqlScriptPath);
+
+            try(PreparedStatement preparedStatement = connection.prepareStatement(hasUserPfpQuery)){
+                preparedStatement.setInt(1, (int) userID);
+
+                ResultSet resultSet = preparedStatement.executeQuery();
+
+                if(resultSet.next()) {
+                    return  resultSet.getBoolean(1);
+                }
+            }  catch (SQLException e){
+                logger.error("SQL execution error: " + e.getMessage(), e);
+                throw new DAOException("sql execution error", e);
+            }
+
+        } catch (SQLException e) {
+        logger.error("Fail to connect to database: " + e.getMessage(), e);
+        throw new DAOException("Fail to connect to database", e);
+        } catch (IOException e){
+            logger.error("Failed to fetch has user pfp sql query -- " + e.getMessage());
+            throw new DAOException("Failed to fetch get insert user pfp sql query", e);
+        }
+
+        return false;
+    }
+
+    @Override
+    public byte[] fetchUserPfp(long userId) throws DAOException {
+        try(Connection connection = CreateDbConnection.getConnection()){
+
+            String sqlScriptPath = UserSqlScriptsPath.FETCH_USER_PFP.getPath();
+            String fetchUserPfp = GetSqlStatement.sqlQueryBuilder(sqlScriptPath);
+
+            try(PreparedStatement preparedStatement = connection.prepareStatement(fetchUserPfp)
+            ) {
+                preparedStatement.setInt(1, (int) userId);
+                ResultSet resultSet = preparedStatement.executeQuery();
+
+                if(resultSet.next()){
+                    return resultSet.getBytes("profile_picture");
+                }
+            } catch (SQLException e){
+                logger.error("SQL execution error: " + e.getMessage(), e);
+                throw new DAOException("sql execution error", e);
+            }
+
+        } catch (SQLException e) {
+            logger.error("Fail to connect to database: " + e.getMessage(), e);
+            throw new DAOException("Fail to connect to database", e);
+        } catch (IOException e){
+            logger.error("Failed to get fetch user pfp sql query -- " + e.getMessage());
+            throw new DAOException("Failed to fetch get insert user pfp sql query", e);
+        }
+        return new byte[0];
+    }
+
+    @Override
+    public int updateUserBio(String bio, int userId) throws DAOException {
+        int updatedRow;
+        try(Connection connection = CreateDbConnection.getConnection()){
+            String sqlScript = UserSqlScriptsPath.UPDATE_USER_BIO.getPath();
+            String updateUserBioQuery = GetSqlStatement.sqlQueryBuilder(sqlScript);
+
+            try(PreparedStatement preparedStatement = connection.prepareStatement(updateUserBioQuery)){
+                BuildSqlStatement.setParameters(preparedStatement, bio, userId);
+                updatedRow = preparedStatement.executeUpdate();
+                logger.info("user bio: {} ", bio);
+            } catch (SQLException e){
+                logger.error("SQL query execution error: " + e.getMessage(), e);
+                throw new DAOException("sql execution error", e);
+            }
+        } catch (SQLException e) {
+            logger.error("Fail to connect to database: " + e.getMessage(), e);
+            throw new DAOException("Fail to connect to database", e);
+        } catch (IOException e){
+            logger.error("Failed to fetch update user bio sql query -- " + e.getMessage());
+            throw new DAOException("Failed to fetch get insert user pfp sql query", e);
+        }
+
+        return updatedRow;
     }
 }
