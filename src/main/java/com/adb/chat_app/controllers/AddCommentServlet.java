b3a8371b84ba11bbba0785797e09dbdd712d8c07
EmmaAdabra <adabraemma@gmail.com>
2025-02-17 07:21:25 +0000

    Add AddCommentServlet class
    - servlet that handles post comment request and response.

---
+++ src/main/java/com/adb/chat_app/controllers/AddCommentServlet.java
@@ -0,0 +1,81 @@
+package com.adb.chat_app.controllers;
+
+import com.adb.chat_app.dao.commentDao.CommentDao;
+import com.adb.chat_app.dao.userdao.UserDao;
+import com.adb.chat_app.dto.CommenterDto;
+import com.adb.chat_app.dto.SessionUserDTO;
+import com.adb.chat_app.exceptions.InputValidationException;
+import com.adb.chat_app.models.Comment;
+import com.adb.chat_app.models.User;
+import com.adb.chat_app.services.CommentService;
+import com.adb.chat_app.utils.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@WebServlet(name = "AddCommentServlet", value = "/add_comment")
+public class AddCommentServlet extends HttpServlet {
+    private static final Logger logger = LoggerFactory.getLogger(AddCommentServlet.class);
+//    private final CommentDao commentDao = new CommentDao();
+//    private final UserDao userDao = new UserDao();
+    private final CommentService commentService = new CommentService(new CommentDao());
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        Response<CommenterDto> jsonResponse;
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            ObjectMapper commentMapper = new ObjectMapper();
+            Comment comment = commentMapper.readValue(request.getReader(), Comment.class);
+            Integer postId = comment.getPostId();
+            String commentText = comment.getComment();
+
+            if(postId != null ){
+                ValidateInputs.validateComment(commentText);
+
+                HttpSession session = request.getSession(false);
+                SessionUserDTO sessionUser = (session != null) ? (SessionUserDTO) session.getAttribute("sessionUser") : null;
+
+                comment.setUserId((sessionUser != null) ? (int) sessionUser.getUserID() : 0);
+
+                jsonResponse = commentService.addComment(comment);
+                response.getWriter().write(objectMapper.writeValueAsString(jsonResponse));
+
+            } else {
+                jsonResponse = new Response<>(
+                        HttpServletResponse.SC_BAD_REQUEST,
+                        "Bad request, no post id"
+                );
+                response.getWriter().write(objectMapper.writeValueAsString(jsonResponse));
+            }
+        } catch (InputValidationException e) {
+            jsonResponse = new Response<>(
+                    HttpServletResponse.SC_BAD_REQUEST,
+                    "Bad request, " + e.getMessage()
+            );
+            response.getWriter().write(objectMapper.writeValueAsString(jsonResponse));
+            logger.error("An error occur while adding comment -- {}", e.getMessage(), e);
+
+        } catch (Exception e){
+            jsonResponse = new Response<>(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Internal server error, fail to add comment");
+            response.getWriter().write(objectMapper.writeValueAsString(jsonResponse));
+            logger.error("An error occur while add comment -- {}", e.getMessage(), e);
+        }
+    }
+}
