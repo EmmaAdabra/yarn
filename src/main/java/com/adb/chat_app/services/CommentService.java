55df12ba3d6362589260c55df9d47cc73b9c46c5
EmmaAdabra <adabraemma@gmail.com>
2025-02-19 08:39:53 +0000

    Modify CommentService class to use new CommentDao saveComment method

--- src/main/java/com/adb/chat_app/services/CommentService.java
+++ src/main/java/com/adb/chat_app/services/CommentService.java
@@ -1,18 +1,15 @@
 package com.adb.chat_app.services;
 
 import com.adb.chat_app.dao.commentDao.CommentDao;
-import com.adb.chat_app.dao.userdao.UserDao;
-import com.adb.chat_app.dto.CommenterDto;
+import com.adb.chat_app.dto.CommentDto;
 import com.adb.chat_app.exceptions.DAOException;
 import com.adb.chat_app.exceptions.UnknownException;
 import com.adb.chat_app.models.Comment;
-import com.adb.chat_app.models.User;
 import com.adb.chat_app.utils.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletResponse;
-import java.util.Date;
 import java.util.Optional;
 
 
@@ -25,21 +22,20 @@ public class CommentService {
     private static final Logger logger = LoggerFactory.getLogger(CommentService.class);
     private CommentDao commentDao;
 
-    public Response<CommenterDto> addComment(Comment comment) throws UnknownException {
-        Response<CommenterDto> serviceResponse;
+    public Response<CommentDto> addComment(Comment comment) throws UnknownException {
+        Response<CommentDto> serviceResponse;
 
         try {
-            int commentId = commentDao.save(comment);
+            Optional<CommentDto> optionalCommentDto = commentDao.saveComment(comment);
 
-            if(commentId != 0){
-                comment.setCommentId(commentId);
-                CommenterDto commenterDto  = createCommenterDto(comment);
+            if(optionalCommentDto.isPresent()){
+                CommentDto commentDto = optionalCommentDto.get();
 
-                serviceResponse = new Response<>(ResponseCode.RESOURCE_CREATED.getCode(), "comment added", commenterDto);
-                logger.info("Comment with ID: {} added to post with ID {}", commentId, comment.getPostId());
+                serviceResponse = new Response<>(ResponseCode.RESOURCE_CREATED.getCode(), "comment added", commentDto);
+                logger.info("Comment with ID: {} added to post with ID {}", commentDto.getCommentId(), comment.getPostId());
             } else {
                 serviceResponse = new Response<>(HttpServletResponse.SC_NOT_FOUND,
-                        "This have be deleted or do no longer exist");
+                        "This have been deleted or do no longer exist");
             }
         } catch (DAOException e) {
             serviceResponse = new Response<>(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Internal server error, fail to add comment");
@@ -48,38 +44,38 @@ public class CommentService {
         return serviceResponse;
     }
 
-    public CommenterDto createCommenterDto(Comment comment) throws DAOException {
-        UserDao userDao = new UserDao();
-        String commenterName = null;
-        String commenterPfpUrl = null;
-        String commenterInitial = null;
-
-
-        if(comment.getUserId() != 0){
-            Optional<User> user = userDao.get(comment.getUserId());
-
-            if(user.isPresent()){
-                User commenter = user.get();
-                commenterName = StringUtil.getUSerFullName(
-                        commenter.getFirstName(),
-                        commenter.getLastName()
-                );
-
-                commenterInitial = StringUtil.getUserInitial( commenter.getFirstName(),
-                        commenter.getLastName());
-
-                commenterPfpUrl = (userDao.hasUserPfp(commenter.getId())) ?
-                        UserUtil.getUserPfpUrl(comment.getUserId()) : null;
-
-            }
-        } else {
-            commenterInitial = "A";
-            commenterName = "Anonymous";
-            commenterPfpUrl = StringUtil.randomAvatarUrl();
-        }
-        comment.setTime(DateUtil.formatPostDate(new Date()));
-        CommenterDto commenterDto = new CommenterDto(commenterName, commenterPfpUrl, comment, commenterInitial);
-
-        return commenterDto;
-    }
+//    public CommentDto createCommenterDto(Comment comment) throws DAOException {
+//        UserDao userDao = new UserDao();
+//        String commenterName = null;
+//        String commenterPfpUrl = null;
+//        String commenterInitial = null;
+//
+//
+//        if(comment.getUserId() != 0){
+//            Optional<User> user = userDao.get(comment.getUserId());
+//
+//            if(user.isPresent()){
+//                User commenter = user.get();
+//                commenterName = StringUtil.getUSerFullName(
+//                        commenter.getFirstName(),
+//                        commenter.getLastName()
+//                );
+//
+//                commenterInitial = StringUtil.getUserInitial( commenter.getFirstName(),
+//                        commenter.getLastName());
+//
+//                commenterPfpUrl = (userDao.hasUserPfp(commenter.getId())) ?
+//                        UserUtil.getUserPfpUrl(comment.getUserId()) : null;
+//
+//            }
+//        } else {
+//            commenterInitial = "A";
+//            commenterName = "Anonymous";
+//            commenterPfpUrl = StringUtil.randomAvatarUrl();
+//        }
+//        String time = (DateUtil.formatPostDate(new Date()));
+//        CommentDto commentDto = new CommentDto(1, commenterName, commenterPfpUrl, commenterInitial, time, comment);
+//
+//        return commentDto;
+//    }
 }
