191797585b86055280ff53b719bc42604b6c00bb
EmmaAdabra <adabraemma@gmail.com>
2025-02-27 02:35:32 +0000

    Change redirection to root route

--- src/main/java/com/adb/chat_app/controllers/LogoutServlet.java
+++ src/main/java/com/adb/chat_app/controllers/LogoutServlet.java
@@ -15,7 +15,7 @@ public class LogoutServlet extends HttpServlet {
             session.invalidate();
         }
 
-        response.sendRedirect("/login");
+        response.sendRedirect("/");
     }
 
     @Override
