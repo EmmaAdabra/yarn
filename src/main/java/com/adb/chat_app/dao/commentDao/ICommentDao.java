d4371c879d3890fd909ce5f1e77977384276e08b
EmmaAdabra <adabraemma@gmail.com>
2025-03-01 00:19:47 +0100

    Deleted unwanted file

--- src/main/java/com/adb/chat_app/dao/commentDao/ICommentDao.java
+++
@@ -1,7 +0,0 @@
-package com.adb.chat_app.dao.commentDao;
-
-import com.adb.chat_app.dao.IDAO;
-import com.adb.chat_app.models.Comment;
-
-public interface ICommentDao extends IDAO<Comment> {
-}
--- src/main/java/com/adb/chat_app/dao/postDao/IPostDao.java
+++
@@ -1,7 +0,0 @@
-package com.adb.chat_app.dao.postDao;
-
-import com.adb.chat_app.dao.IDAO;
-import com.adb.chat_app.models.Post;
-
-public interface IPostDao extends IDAO<Post> {
-}
