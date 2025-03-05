6bd4943ec13a8c40885c8442e425efae77684032
EmmaAdabra <adabraemma@gmail.com>
2025-03-05 09:34:56 +0100

    Modify ICommentDao interface
    - Stopped extending IDAO interface

--- src/main/java/com/adb/any_post/dao/commentDao/ICommentDao.java
+++ src/main/java/com/adb/any_post/dao/commentDao/ICommentDao.java
@@ -1,7 +1,14 @@
 package com.adb.any_post.dao.commentDao;
 
-import com.adb.any_post.dao.IDAO;
+import com.adb.any_post.dto.CommentDto;
+import com.adb.any_post.exceptions.DAOException;
 import com.adb.any_post.models.Comment;
 
-public interface ICommentDao extends IDAO<Comment> {
+import java.util.List;
+import java.util.Optional;
+
+public interface ICommentDao {
+    List<CommentDto> getPostComments(int postId) throws DAOException;
+    Optional<CommentDto> saveComment(Comment comment) throws DAOException;
+    int deleteComment(int commentId, int ownerId) throws DAOException;
 }
