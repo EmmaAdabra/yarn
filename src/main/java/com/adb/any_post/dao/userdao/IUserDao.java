8c0c21f4f491854174fd89c6b88551ed9ab3f85c
EmmaAdabra <adabraemma@gmail.com>
2025-03-05 09:27:50 +0100

    Modify IUserDao interface
    - Stop extending IDAO interface

--- src/main/java/com/adb/any_post/dao/userdao/IUserDao.java
+++ src/main/java/com/adb/any_post/dao/userdao/IUserDao.java
@@ -1,12 +1,14 @@
 package com.adb.any_post.dao.userdao;
 
-import com.adb.any_post.dao.IDAO;
 import com.adb.any_post.exceptions.DAOException;
 import com.adb.any_post.models.User;
 
 import javax.servlet.http.Part;
+import java.util.Optional;
 
-public interface IUserDao extends IDAO<User> {
+public interface IUserDao{
+    Optional<User> get(long userId) throws DAOException;
+    int save(User user) throws DAOException;
     User findUserByEmail(String email) throws DAOException;
     boolean saveUserPfp(long userId, Part imagePart) throws DAOException;
     boolean hasUserPfp(long userID) throws DAOException;
