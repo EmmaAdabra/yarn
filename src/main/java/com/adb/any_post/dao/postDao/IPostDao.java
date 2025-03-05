36e62732b0f666e325cce5468bedfde000f14064
EmmaAdabra <adabraemma@gmail.com>
2025-03-05 09:24:03 +0100

    Modify IPostDao interface
    - Stop extending IDAO generic DAO interface

--- src/main/java/com/adb/any_post/dao/postDao/IPostDao.java
+++ src/main/java/com/adb/any_post/dao/postDao/IPostDao.java
@@ -1,7 +1,13 @@
 package com.adb.any_post.dao.postDao;
 
-import com.adb.any_post.dao.IDAO;
+import com.adb.any_post.dto.PostDto;
+import com.adb.any_post.exceptions.DAOException;
 import com.adb.any_post.models.Post;
 
-public interface IPostDao extends IDAO<Post> {
+import java.util.List;
+
+public interface IPostDao {
+    List<PostDto> getAllPost() throws DAOException;
+    int save(Post post) throws DAOException;
+    int deletePost(int postId, int ownerId) throws DAOException;
 }
