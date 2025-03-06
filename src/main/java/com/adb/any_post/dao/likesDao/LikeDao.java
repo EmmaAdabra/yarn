8c9f8142f3ad5c97d7a3da28f693467f3c0b59ff
EmmaAdabra <adabraemma@gmail.com>
2025-03-07 00:03:26 +0100

    Update LikeDao class.
    - Add getUserLikedPosts method
    - Get user Id, return a collection of posts
    liked by user

--- src/main/java/com/adb/any_post/dao/likesDao/LikeDao.java
+++ src/main/java/com/adb/any_post/dao/likesDao/LikeDao.java
@@ -1,6 +1,5 @@
 package com.adb.any_post.dao.likesDao;
 
-import com.adb.any_post.dao.commentDao.CommentSqlScriptsPath;
 import com.adb.any_post.exceptions.DAOException;
 import com.adb.any_post.models.Like;
 import com.adb.any_post.utils.BuildSqlStatement;
@@ -14,6 +13,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LikeDao {
     private static final Logger logger = LoggerFactory.getLogger(LikeDao.class);
@@ -52,5 +53,39 @@ public class LikeDao {
         }
 
         return likeId;
+    }
+
+    public List<Integer> getUserLikedPosts(int user_id) throws DAOException{
+        List<Integer> likedPosts = new ArrayList<>();
+
+        try(Connection connection = CreateDbConnection.getConnection()){
+            String sqlScriptPath = LikeSqlScriptsPath.GET_USER_LIKED_POSTS.getPath();
+            String getUserLikedPostQuery = GetSqlStatement.sqlQueryBuilder(sqlScriptPath);
+
+            try(PreparedStatement preparedStatement = connection.prepareStatement(getUserLikedPostQuery)){
+
+                preparedStatement.setInt(1, user_id);
+
+                ResultSet resultSet = preparedStatement.executeQuery();
+
+                while (resultSet.next()){
+                    int postId = resultSet.getInt("post_id");
+                    likedPosts.add(postId);
+                }
+
+            } catch (SQLException e){
+                logger.error("SQL query execution error: " + e.getMessage(), e);
+                throw new DAOException("sql execution error", e);
+            }
+
+        } catch (SQLException e) {
+            logger.error("Fail to connect to database: " + e.getMessage(), e);
+            throw new DAOException("Fail to connect to database", e);
+        } catch (IOException e){
+            logger.error("Failed to fetch get user liked posts sql query -- " + e.getMessage(), e);
+            throw new DAOException("Failed to fetch,  get user liked posts sql query", e);
+        }
+
+        return likedPosts;
     }
 }
