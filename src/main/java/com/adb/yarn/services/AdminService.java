9117d12feceb70c464c01c646fb798e989247ab5
EmmaAdabra <adabraemma@gmail.com>
2025-03-29 13:17:27 +0000

    Refactor AdminService.java
    - Remove admin IDs implementation
    - Ensure it focus on business logic functionality

--- src/main/java/com/adb/yarn/services/AdminService.java
+++ src/main/java/com/adb/yarn/services/AdminService.java
@@ -1,34 +1,13 @@
 package com.adb.yarn.services;
 
-import com.adb.yarn.dao.admindao.AdminDao;
+import com.adb.yarn.cache.AdminCache;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashSet;
-
 public class AdminService {
     private static final Logger logger = LoggerFactory.getLogger(AdminService.class);
-    private final static HashSet<Integer> adminIdList = new HashSet<>();
-
-    public static void loadAdminId(){
-        try{
-            HashSet<Integer>  idList = new AdminDao().getAdminId();
-
-            if(!idList.isEmpty()){
-                adminIdList.addAll(idList);
-            } else {
-                logger.warn("No admin found, setting default admin id to 27");
-                adminIdList.add(27);
-            }
-
-        } catch (Exception e) {
-            logger.error("Failed to fetch all admin Id from database - {}", e.getMessage(), e);
-            logger.warn("Setting default admin id to 27");
-            adminIdList.add(27);
-        }
-    }
 
     public static boolean isAdmin(int userId){
-        return adminIdList.contains(userId);
+        return AdminCache.contains(userId);
     }
 }
